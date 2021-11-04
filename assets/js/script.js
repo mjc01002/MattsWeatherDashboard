61a200ee202aa26e1ba6c0fa62963f8e6572e38b
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-11-04 18:52:48 -0400

    updated to get date and other data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 var cityInputEl = document.querySelector("#city");
 
 var formSubmitHandler = function() {
+  $(document).ready(function(){
+    var date = moment().format("  (MM/DD/YYYY)");
+    $("#currentDay").append(date);
+    });
+
 var city = cityInputEl.value.trim();
     if (city) {
       getCord(city);
       cityInputEl.value = "";
+      $("#currentDay").append(city);
+
     } else {
         alert("Please enter a city username");
       }
@@ -23,11 +30,35 @@ fetch(apiCall).then(function(response) {
     console.log(latitude)
     console.log(longitude)
 
-    var apiCall2 ="https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=hourly,daily&appid=1c91db0624deadc30a568dac3fdff159";
+    var apiCall2 ="https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=1c91db0624deadc30a568dac3fdff159";
  
   fetch(apiCall2).then(function(response) {
   response.json().then(function(data){
     console.log(data);
+
+    var tempEl = document.createElement("temp");
+    var temp =" " + data.current.temp +"\xB0";
+    tempEl.append(temp);
+    document.getElementById("temp").append(tempEl);
+
+    var windEl = document.createElement("wind");
+    var wind =" " + data.current.wind_speed +"MPH";
+    windEl.append(wind);
+    document.getElementById("wind").append(windEl);
+
+    var humidityEl = document.createElement("humidity");
+    var humidity =" " + data.current.humidity +"%";
+    humidityEl.append(humidity);
+    document.getElementById("humidity").append(humidityEl);
+
+    var uvEl = document.createElement("UV");
+    var uv =" " + data.current.uvi;
+    uvEl.append(uv);
+    document.getElementById("UV").append(uvEl);
+
+   //var gifImg = document.createElement('img');
+    // gifImg.setAttribute('src', response.data[4].images.fixed_height.url);
+    // responseContainerEl.appendChild(gifImg);
   
   })
 })
