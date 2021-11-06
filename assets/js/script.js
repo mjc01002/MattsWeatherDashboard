99194712b375d3ded797b0cc583839391a5a48ca
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-11-06 07:38:14 -0400

    working on recent buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+  
 var recentCities=[];
 
 var cityInputEl = document.querySelector("#city");
@@ -29,7 +30,7 @@ var city = cityInputEl.value.trim();
 
 var getCord = function(city) {
 
-var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=1c91db0624deadc30a568dac3fdff159";
+var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=34497f5e5244ea1659db421cd1f2a1de";
 
 fetch(apiCall).then(function(response) {
   response.json().then(function(data){
@@ -40,7 +41,7 @@ fetch(apiCall).then(function(response) {
     console.log(latitude)
     console.log(longitude)
 
-    var apiCall2 ="https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=1c91db0624deadc30a568dac3fdff159";
+    var apiCall2 ="https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=34497f5e5244ea1659db421cd1f2a1de";
  
   fetch(apiCall2).then(function(response) {
   response.json().then(function(data){
@@ -126,49 +127,145 @@ fetch(apiCall).then(function(response) {
 
 });
 });
-    
+      recentCities.push(city);
+      localStorage.setItem("city", JSON.stringify(recentCities));
+
       var button = document.createElement("button");
       button.innerHTML = city;
       button.className = "recentBtn";
       button.onclick = function(){
-        getCord(city);
+        recent(city);
       };
       document.getElementById("recentButtons").append(button);
-
-      //localStorage.setItem("function", getCord());
-      recentCities.push(city);
-      localStorage.setItem("city", JSON.stringify(recentCities));
-      
+ 
 };
-
-//Retrieve recent cities from local storage and create buttons
-   var retrievedCities = localStorage.getItem("city");
-  recentCities = JSON.parse(retrievedCities);
-
-  for(i=0; i < recentCities[i].length; i ++) {
-
-  var button = document.createElement("button");
-  button.innerHTML = recentCities[i];
-  button.className = "btn";
-  button.onclick = function(){
-    getCord(recentCities[i]);
-  };
-
-  document.getElementById("recentButtons").append(button);
-  }
-
-
-
 
 console.log(recentCities);
 
 
+//Retrieve recent cities from local storage and create buttons
+var retrievedCities = localStorage.getItem("city");
+
+console.log(recentCities);
+
+// this will check to see if retrievedCities has a value
+if(retrievedCities){
+recentCities = JSON.parse(retrievedCities);
+}
+for(i=0; i < recentCities[i].length; i ++) {
+city = recentCities[i];
+var button = document.createElement("button");
+button.innerHTML = recentCities[i];
+button.className = "btn";
+button.onclick = function(){
+  recent(city);
+};
+
+document.getElementById("recentButtons").append(button);
+}
 
 
 
+// allows the user to click on recent button after it created. This is used before the website is refreshed.
+function recent(city) {
+
+  var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=34497f5e5244ea1659db421cd1f2a1de";
+
+  fetch(apiCall).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+
+      var latitude = data.coord.lat;
+      var longitude = data.coord.lon;
+      console.log(latitude);
+      console.log(longitude);
+
+      var apiCall2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=34497f5e5244ea1659db421cd1f2a1de";
+
+      fetch(apiCall2).then(function (response) {
+        response.json().then(function (data) {
+          console.log(data);
+
+          // Current Forecast
+          var tempEl = document.createElement("temp");
+          var temp = " " + data.current.temp + "\xB0" + "F";
+          tempEl.append(temp);
+          document.getElementById("temp").append(tempEl);
+
+          var windEl = document.createElement("wind");
+          var wind = " " + data.current.wind_speed + "MPH";
+          windEl.append(wind);
+          document.getElementById("wind").append(windEl);
+
+          var humidityEl = document.createElement("humidity");
+          var humidity = " " + data.current.humidity + "%";
+          humidityEl.append(humidity);
+          document.getElementById("humidity").append(humidityEl);
+
+          var uvEl = document.createElement("UV");
+          var uv = " " + data.current.uvi;
+          uvEl.append(uv);
+          document.getElementById("UV").append(uvEl);
+
+          var iconEl = document.createElement("icon");
+          var icon = data.current.weather[0].icon;
+          iconEl.append(icon);
+          document.getElementById("icon").append(icon);
+          document.getElementById("icon").setAttribute("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
+
+          // set color on uv index
+          if (uv <= 3) {
+            $("#UV").addClass("uvLow");
+          } else if (uv <= 4 || uv <= 6) {
+            $("#UV").addClass("uvMed");
+          } else if (uv <= 7) {
+            $("#UV").addClass("uvHigh");
+          } else { };
 
 
+          //Five Day Forecast Section
+          for (i = 1; i < 6; i++) {
+            var y = ("date" + i);
+
+            var dateEl = document.createElement(y);
+            var date1 = data.daily[i].dt * 1000;
+            var dateObject = new Date(date1);
+            var dateFormat = dateObject.toLocaleDateString("en-US");
+            dateEl.append(dateFormat);
+            document.getElementById(y).append(dateEl);
+
+            var w = ("icon" + i);
+            var iconEl = document.createElement(w);
+            var icon = data.daily[i].weather[0].icon;
+            iconEl.append(icon);
+            document.getElementById(w).append(icon);
+            document.getElementById(w).setAttribute("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png");
+
+            var u = ("temp" + i);
+            var temp1El = document.createElement(u);
+            var temp1 = " " + data.daily[i].temp.day + "\xB0" + "F";
+            temp1El.append(temp1);
+            document.getElementById(u).append(temp1El);
+
+            var v = ("wind" + i);
+            var wind1El = document.createElement(v);
+            var wind1 = " " + data.daily[i].wind_speed + " MPH";
+            wind1El.append(wind1);
+            document.getElementById(v).append(wind1El);
+
+            var t = ("humidity" + i);
+            var humidity1El = document.createElement(t);
+            var humidity1 = " " + data.daily[i].humidity + " %";
+            humidity1El.append(humidity1);
+            document.getElementById(t).append(humidity1El);
+
+          }
+        });
+      });
+
+    });
+  });
 
 
-
+}
 
